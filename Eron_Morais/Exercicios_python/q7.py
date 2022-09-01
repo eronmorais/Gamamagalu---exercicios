5f152d1d841eb895786e7fdd339c8c6dfa6336fd
Eron Morais <eron.cruz@luizalabs.com>
2022-09-01 00:00:11 -0300

    Inclui o m² no exercicio

--- Eron_Morais/Exercicios_python/q7.py
+++ Eron_Morais/Exercicios_python/q7.py
@@ -1,3 +1,3 @@
 lado = input("Digite o tamanho do lado do quadrado: ")
 area = float(lado) * float(lado)
-print("O dobro da area do quadrado é:",area *2)
+print("O dobro da area do quadrado é:",area *2,'m²')

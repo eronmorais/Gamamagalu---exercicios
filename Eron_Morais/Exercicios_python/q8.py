4eab1e5593ed1434cdc3aa8d47636c97530ddcca
Eron Morais <eron.cruz@luizalabs.com>
2022-09-01 00:12:37 -0300

    Exercicio 8 feito

---
+++ Eron_Morais/Exercicios_python/q8.py
@@ -0,0 +1,4 @@
+g_hora = float(input("Quanto você ganha por hora: "))
+h_mes = float(input("Horas trabalhadas no mês: "))
+salario_mes = (g_hora) * (h_mes)
+print('O seu salario esse mês é:',salario_mes)

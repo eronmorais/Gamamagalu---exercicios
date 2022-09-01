4adbe7f82c417f18ca199bda87a89fe788a30420
Eron Morais <eron.cruz@luizalabs.com>
2022-09-01 01:17:06 -0300

    Exercicios 12 a 15 feitos

---
+++ Eron_Morais/Exercicios_python/q12.py
@@ -0,0 +1,2 @@
+altura = float(input("Digite sua altura: "))
+print("Seu peso ideal é:",(72.7*altura)-58)
---
+++ Eron_Morais/Exercicios_python/q13.py
@@ -0,0 +1,3 @@
+altura = float(input("Digite sua altura: "))
+print("1. Peso ideal para homens é:",(72.7*altura)-58)
+print("2. Peso ideal para mulheres é:", (62.1*altura)-44.7)

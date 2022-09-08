695b352b375bc0f46a3386bbf9cfdd20cf2cadc4
Eron Morais <eron.cruz@luizalabs.com>
2022-09-08 15:09:12 -0300

    exercicios 1 e 2 listas

---
+++ Eron_Morais/Exercicios_python/04ExerciciosLista/q02.py
@@ -0,0 +1,10 @@
+#Faça um Programa que leia um vetor de 10 números reais e mostre-os na ordem inversa.
+
+numero = []
+print('Digite 10 numeros: ')
+for i in range(5):
+    numero.append(float(input('Numero: ')))
+    numero.reverse()
+print(numero)
+    
+    
